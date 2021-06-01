c7126bbfc960d04d17132c4c07500af872cd98d9
Adsel <marcin.radwan08@gmail.com>
2021-06-01 20:54:32 +0200

    feat: changing user's password

--- src/main/java/com/example/PhysioCenter/service/UserService.java
+++ src/main/java/com/example/PhysioCenter/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.PhysioCenter.service;
 
 import com.example.PhysioCenter.domain.dto.users.LoginRequestDto;
+import com.example.PhysioCenter.domain.dto.users.UpdatePasswordDto;
 import com.example.PhysioCenter.domain.exceptions.LoginDuplicatedException;
 import com.example.PhysioCenter.domain.exceptions.PatientNotCreatedException;
 import com.example.PhysioCenter.domain.dto.users.CreatePatientUserDto;
@@ -12,4 +13,5 @@ public interface UserService {
     UserDto createPatientUser(CreatePatientUserDto createPatientUserDto) throws PatientNotCreatedException, UserNotCreatedException, LoginDuplicatedException;
     UserDto loginPatient(LoginRequestDto loginRequestDto) throws WrongLoginDataException;
     UserDto loginPhysio(LoginRequestDto loginRequestDto) throws WrongLoginDataException;
+    boolean updateUserPassword(UpdatePasswordDto userPassword, Long id);
 }
