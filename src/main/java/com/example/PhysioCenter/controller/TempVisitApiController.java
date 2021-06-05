9ee1cfe9a9c0a9713d3925a69d75c120c45ced5d
Adsel <marcin.radwan08@gmail.com>
2021-06-05 10:11:58 +0200

    feat: SMTP mail

--- src/main/java/com/example/PhysioCenter/controller/TempVisitApiController.java
+++ src/main/java/com/example/PhysioCenter/controller/TempVisitApiController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 @Controller
---
+++ src/main/java/com/example/PhysioCenter/service/serviceImpl/SMTPService.java
@@ -0,0 +1,44 @@
+package com.example.PhysioCenter.service.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SMTPService {
+    @Autowired
+    private JavaMailSender mailSender;
+    private final String TEST_EMAIL_FROM = "marcin.radwan08@gmail.com";
+    private final String TEST_EMAIL_TO = "marcin.radwan08@gmail.com";
+    private final boolean SEND_MESSAGE = false;
+
+    private void sendEmail(String subject, String HTMLtext) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setSubject(subject);
+        helper.setFrom(TEST_EMAIL_FROM);
+        helper.setTo(TEST_EMAIL_TO);
+        helper.setText(HTMLtext, true);
+
+        if (SEND_MESSAGE) {
+            mailSender.send(message);
+        }
+    }
+
+    public void sendEmailAboutCancelVisit(String date, String timeStart, String timeStop) throws MessagingException {
+        String title = "Visit cancelled";
+        String msg = "Your visit in " + date + " at " + timeStart + " to " + timeStop + "has been cancelled.";
+        sendEmail(title, msg);
+    }
+
+    public void sendEmailAboutRegisterVisit(String date, String timeStart, String timeStop) throws MessagingException {
+        String title = "Visit registered";
+        String msg = "Your visit in " + date + " at " + timeStart + " to " + timeStop + "has been registered.";
+        sendEmail(title, msg);
+    }
+}
